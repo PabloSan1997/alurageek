01ffcc4810ef7ba45c913ec8fb8d3716ac7a1879
PabloSan1997 <jos_pab_997@hotmali.com>
2023-07-23 16:15:30 -0600

    Se agregaron estilos y funcionalidad al formulario para contactanos

--- client/src/components/FomularioContacto.tsx
+++ client/src/components/FomularioContacto.tsx
@@ -1,22 +1,54 @@
 import React from 'react'
+import '../estilos/formularioContacto.scss'
 
 export function FomularioContacto() {
-    const [entradas, setEntradas] = React.useState({nombre:'', mensaje:''});
-    const mandar = (e:React.FormEvent<HTMLFormElement>) =>{
-        e.preventDefault();
+    const [entradas, setEntradas] = React.useState({ nombre: '', mensaje: '' });
+    const [estilos, setEstilo] = React.useState({
+        nombre:{},
+        mensaje:{}
     }
-  return (
-    <form  className="formularioContacto" onSubmit={mandar}>
-        <h2 className='titulo_formulario'>Hable con nosotros</h2>
-        <div className="datos_formulario">
-            <label htmlFor="">Nombre</label>
-            <input type="text" />
+    );
+    const mandar = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if(!entradas.nombre || !entradas.mensaje){
+            alert('Llene todos los campos');
+            const nombre = !entradas.nombre?{color:'red'}:{};
+            const mensaje = !entradas.mensaje?{color:'red'}:{};
+            setEstilo({nombre, mensaje});
+        }else{
+            alert('Se mandó el mensaje');
+            setEstilo({nombre:{}, mensaje:{}});
+        }
+    }
+    const setNombre = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEntradas({ ...entradas, nombre: e.target.value });
+    }
+    const setMensaje = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setEntradas({ ...entradas, mensaje: e.target.value });
+    }
+    return (
+        <form className="formularioContacto" onSubmit={mandar}>
+            <h2 className='titulo_formulario'>Hable con nosotros</h2>
+            <div className="datos_formulario">
+                <label htmlFor="nombre" style={estilos.nombre}>Nombre</label>
+                <input
+                    type="text"
+                    onChange={setNombre}
+                    value={entradas.nombre}
+                    id="nombre"
+                    className='entrada' />
             </div>
-        <div className="datos_formulario">
-            <label htmlFor="">Escribe su mensaje</label>
-            <textarea name="" id=""></textarea>
+            <div className="datos_formulario">
+                <label htmlFor="mensaje" style={estilos.mensaje}>Escribe su mensaje</label>
+                <textarea
+                    id="mensaje"
+                    className='entrada'
+                    value={entradas.mensaje}
+                    onChange={setMensaje}
+                    ></textarea>
             </div>
-        <button className='boton' type='submit'>Enviar</button>
-    </form>
-  )
+            <button className='boton' type='submit'>Enviar mensaje</button>
+        </form>
+    )
 }
