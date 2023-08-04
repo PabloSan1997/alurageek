e3e7806ad487ccf9fee3fb726d9daaa32b0fc78d
PabloSan1997 <jos_pab_997@hotmali.com>
2023-08-04 10:40:47 -0600

    Se agrego informacion al footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,11 +1,12 @@
 import '../estilos/footer.scss';
 
 //Pie de pagina para mandar al usuario al github del proyecto
-export  function Footer() {
+export function Footer() {
   return (
     <footer>
-        <p>Pagina desarrollada por <a href="https://github.com/PabloSan1997/alurageek" target="_blank">Jose Pablo</a></p>
-        <span>2023</span>
+      <span className='texto'>La primera solicitud puede tardar varios minutos en cargar</span>
+      <p>Pagina desarrollada por <a href="https://github.com/PabloSan1997/alurageek" target="_blank">Jose Pablo</a></p>
+      <span className='fecha'>2023</span>
     </footer>
   )
 }
